5f46c46feb08cc1c8f617fd12a6d9cfd4e47d8c8
Rhuan S. Hianc <rhuanhianc123@gmail.com>
2021-11-12 15:13:54 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Chess_Project_Study_Java
+ I make this project to Study Java POO
---
+++ src/application/ProgramMain.java
@@ -0,0 +1,9 @@
+package application;
+
+public class ProgramMain {
+
+	public static void main(String[] args) {
+		System.out.println("Teste Inicial");
+	}
+
+}

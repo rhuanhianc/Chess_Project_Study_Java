3088319e3e1da19e3545ec3d8c8a8faea18ff644
Rhuan S. Hianc <rhuanhianc123@gmail.com>
2021-12-11 20:14:51 -0300

    Update ProgramMain.java

--- src/application/ProgramMain.java
+++ src/application/ProgramMain.java
@@ -15,13 +15,7 @@ public class ProgramMain {
 //		ChessMatch chessMatch = new ChessMatch();
 //		UI.printBoard(chessMatch.getPieces());
 		
-
-		String[] teste ={"a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"};
-		
-		for(int i = 0; i < teste.length; i++) {
-			teste[i] = teste[i].toUpperCase();
-			System.out.println(teste[i]);
-		}
+//		Board board = new Board(8, 8);
 	}
 
 }

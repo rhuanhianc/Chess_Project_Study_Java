b7c1303471c41e784e1a563270c92f3f042327f6
Rhuan S. Hianc <rhuanhianc123@gmail.com>
2021-12-11 22:20:25 -0300

    Update ChessPosiotion.java

--- src/chess/ChessPosiotion.java
+++ src/chess/ChessPosiotion.java
@@ -1,5 +1,7 @@
 package chess;
 
+import boardgame.Position;
+
 public class ChessPosiotion {
 	
 	private char column;
@@ -7,10 +9,27 @@ public class ChessPosiotion {
 	
 	public ChessPosiotion(char column, int row) {
 		if(column < 'a' || column > 'h' || row < 1 || row > 8) {
-			throw new ChessException("Errosr instanciando chess posrtion n„o esta um valor valido");
+			throw new ChessException("Errosr instanciando chess posrtion n√£o esta um valor valido");
 		}
 		this.column = column;
 		this.row = row;
 	}
-	
+	protected Position toPosition() {
+		return new Position(8 - row, column - 'a');
+	}
+	protected static ChessPosiotion fromPosition(Position position) {
+		return new ChessPosiotion((char)('a' + position.getColumn()), 8 - position.getRow());
+	}
+
+	@Override
+	public String toString(){
+		return "" + column + row;
+	}
+	public char getColumn() {
+		return column;
+	}
+	public int getRow() {
+		return row;
+	}
+
 }

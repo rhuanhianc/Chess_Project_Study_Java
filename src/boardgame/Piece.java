0082c5969f3c99eb213ebabde06dee98ecafd1ac
Rhuan Hianc <rhuanhianc123@gmail.com>
2022-01-12 20:50:22 -0300

    Update Piece.java

--- src/boardgame/Piece.java
+++ src/boardgame/Piece.java
@@ -13,5 +13,21 @@ public abstract class Piece {
 	protected Board getBoard() {
 		return board;
 	}
+	public abstract boolean[][] possibleMoves();
+	public boolean possibleMove(Position position) {
+		return possibleMoves()[position.getRow()][position.getColumn()];
+	}
+
+	public boolean isThereAnyPossibleMove() {
+		boolean[][] mat = possibleMoves();
+		for (int i = 0; i < mat.length; i++) {
+			for (int j = 0; j < mat.length; j++) {
+				if (mat[i][j]) {
+					return true;
+				}
+			}
+		}
+		return false;
+	}
 
 }

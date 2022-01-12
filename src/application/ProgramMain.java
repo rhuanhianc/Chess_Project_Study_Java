815e5ce6deee36162d4e41b9d513de942d6ef16a
Rhuan Hianc <rhuan.hianc@el.com.br>
2022-01-12 16:27:40 -0300

    Update ProgramMain.java

--- src/application/ProgramMain.java
+++ src/application/ProgramMain.java
@@ -7,14 +7,26 @@ import java.util.List;
 
 import boardgame.Board;
 import chess.ChessMatch;
+import chess.ChessPiece;
+import chess.ChessPosiotion;
+import java.util.Scanner;
+
 
 public class ProgramMain {
 
 	public static void main(String[] args) {
-
+		Scanner sc = new Scanner(System.in);
 		ChessMatch chessMatch = new ChessMatch();
+		while (true){
 		UI.printBoard(chessMatch.getPieces());
-		
+		System.out.println();
+		System.out.print("Source: ");
+		ChessPosiotion source = UI.readChessPosition(sc);
+		System.out.println();
+		System.out.print("Target: ");
+		ChessPosiotion target = UI.readChessPosition(sc);
+		ChessPiece capturedPiece = chessMatch.perfomChessMove(source, target);
+	}
 //		Board board = new Board(8, 8);
 	}
 

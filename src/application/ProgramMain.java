29ba5d9c2e48c96080c84530ee05ec7b516fc779
Rhuan Hianc <rhuan.hianc@el.com.br>
2022-01-12 16:39:38 -0300

    Limpar a tela

--- src/application/ProgramMain.java
+++ src/application/ProgramMain.java
@@ -3,6 +3,7 @@ package application;
 
 
 import java.util.ArrayList;
+import java.util.InputMismatchException;
 import java.util.List;
 
 import boardgame.Board;
@@ -10,6 +11,7 @@ import chess.ChessMatch;
 import chess.ChessPiece;
 import chess.ChessPosiotion;
 import java.util.Scanner;
+import chess.ChessException;
 
 
 public class ProgramMain {
@@ -17,17 +19,28 @@ public class ProgramMain {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		ChessMatch chessMatch = new ChessMatch();
-		while (true){
-		UI.printBoard(chessMatch.getPieces());
-		System.out.println();
-		System.out.print("Source: ");
-		ChessPosiotion source = UI.readChessPosition(sc);
-		System.out.println();
-		System.out.print("Target: ");
-		ChessPosiotion target = UI.readChessPosition(sc);
-		ChessPiece capturedPiece = chessMatch.perfomChessMove(source, target);
+		while (true){		
+		try{
+			UI.clearScreen();
+			UI.printBoard(chessMatch.getPieces());
+			System.out.println();
+			System.out.print("Source: ");
+			ChessPosiotion source = UI.readChessPosition(sc);
+			System.out.println();
+			System.out.print("Target: ");
+			ChessPosiotion target = UI.readChessPosition(sc);
+			ChessPiece capturedPiece = chessMatch.perfomChessMove(source, target);
+		}catch(ChessException e) {
+			System.out.println(e.getMessage());
+			sc.nextLine();
+		}catch(InputMismatchException e) {
+			System.out.println(e.getMessage());
+			sc.nextLine();
+		}
+
+		}
 	}
 //		Board board = new Board(8, 8);
 	}
 
-}
+

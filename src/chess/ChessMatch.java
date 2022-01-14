c573763c80b3ffdc653972e27a7642b1e82287e9
Rhuan Hianc <rhuanhianc123@gmail.com>
2022-01-14 20:05:04 -0300

    Update ChessMatch.java

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -9,12 +9,20 @@ import boardgame.Piece;
 public class ChessMatch {
 	
 	private Board board;
-	
+	private int turn;
+	private Color currentPlayer;
 	public ChessMatch() {
 		board = new Board(8, 8);
+		turn = 1;
+		currentPlayer = Color.WHITE;
 		initialSetup();
 	}
-
+	public int getTurn(){
+		return turn;
+	}
+	public Color getCurrentPlayer(){
+		return currentPlayer;
+	}
 	public ChessPiece[][] getPieces(){
 		ChessPiece[][] mat = new ChessPiece[board.getRows()][board.getColumns()];
 		for(int i=0; i<board.getRows(); i++) {
@@ -35,7 +43,7 @@ public class ChessMatch {
 		validateSourcePosition(source);
 		validateTargetPosition(source, target);
 		Piece capturedPiece = makeMove(source, target);
-		
+		nextTurn();
 		return (ChessPiece) capturedPiece;
 	}
 
@@ -61,7 +69,10 @@ public class ChessMatch {
 	private void placeNewPiece(char column, int row, ChessPiece piece) {
 		board.placePiece(piece, new ChessPosiotion(column, row).toPosition());
 	}
-
+	private void nextTurn(){
+		turn++;
+		currentPlayer = (currentPlayer == Color.WHITE) ? Color.BLACK : Color.WHITE;
+	}
 	private void initialSetup() {
 		placeNewPiece('b', 6,new Rook(board, Color.WHITE));
 		placeNewPiece('e', 8,new King(board, Color.BLACK));
